d72918d4d9f10092239b0fc41c792f81fdc10ad8
Magno <magno.luque@upch.pe>
2024-05-16 02:34:44 -0500

    change

--- Actvidad_17/myanswerprob1.py
+++ Actvidad_17/myanswerprob1.py
@@ -1,31 +1,37 @@
 # Importamos librerías
 import re  # módulo que proporciona operaciones de coincidencia
 import random as rd
+import time # módulo que permite trabajar con el tiempo
 
 # Modificación de funciones 
 def servidor(mensaje):
     """Procesa los mensajes enviados por el cliente según el comando especificado."""
-    mensaje = mensaje.upper()  
-    if re.match("^[A-Z:0-9\s+]+$",mensaje):
+    mensaje = mensaje.upper() # convertimos el mensaje a mayúsculas
+    retraso = rd.uniform(0,5) # simulamos retrasos de Red
+    print("------")
+    print("Procesando solicitud...")
+    time.sleep(retraso)
+    if re.match("^[A-Z:0-9\s+]+$",mensaje): # validación de mensajes
         comando, _, contenido = mensaje.partition(':')
         
         if comando.strip() == 'ECO':
-            return contenido.strip()
+            return contenido.strip(), retraso
         elif comando.strip() == 'REVERSO':
-            return contenido.strip()[::-1]
-        elif comando.strip() == 'SUMAR':
+            return contenido.strip()[::-1], retraso
+        elif comando.strip() == 'SUMAR':  # sumar números
             numeros = [int(i) for i in contenido.split('+')]
             suma = sum(numeros)
-            return suma
+            return suma, retraso
         else:
-            return "Comando no reconocido."
+            return "Comando no reconocido.", retraso
     else:
-        return "El mensaje contiene caracteres no permitidos."
+        return "El mensaje contiene caracteres no permitidos.", retraso
     
 def cliente(mensaje):
     """Enviar los mensajes al servidor y procesar la respuesta."""
-    respuesta = servidor(mensaje)
+    respuesta, retraso = servidor(mensaje)
     print(f'Servidor dice: {respuesta}')
+    print(f'{retraso:.2f} segundos en responder...')
     
 def main():
     cliente("eco: Hola Mundo")
